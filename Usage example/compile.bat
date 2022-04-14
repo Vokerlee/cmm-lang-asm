ee478d4d4e50151e6a24f4e6448c6951b98f5a37
Vokerlee <vokerlee@gmail.com>
2022-04-14 23:22:42 +0300

    Remove all .exe files and examples based on WIndows OS

--- Usage example/compile.bat
+++
@@ -1,8 +0,0 @@
-"frontend_cmm.exe" program.cmm       program.tree
-"optimizer.exe"    program.tree      program_optm.tree
-"backend.exe"      program_optm.tree program.vasm
-
-"asm.exe"  program.vasm program.ncpu
-"nCPU.exe" program.ncpu
-
-pause
